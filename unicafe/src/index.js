3302a87a08ae1851e9e412ba95b3b17819798c16
MikkoKarvonen <mkarvonen34@gmail.com>
2019-03-17 06:47:40 +0200

    Lisätty tehtävä 1.10

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistic = ({text, value, pre}) => {
+    return(
+        <div>
+            <p>{text} {value} {pre}</p>
+        </div>
+    )
+}
+
 const Statistics = ({good, bad, neutral}) => {
     const total = good + bad + neutral;
     const statistics = (total > 0) ? (
         <div>
-            <p>hyvä {good}</p>
-            <p>neutraali {neutral}</p>
-            <p>huono {bad}</p>
-            <p>yhteensä {good + bad + neutral}</p>
-            <p>keskiarvo {(good + bad * (-1) + neutral * 0) / (good + bad + neutral)}</p>
-            <p>positiivisia {good / (good + bad + neutral) * 100} %</p>
+            <Statistic text="hyvä" value={good} />
+            <Statistic text="neutraali" value={neutral} />
+            <Statistic text="huono" value={bad} />
+            <Statistic text="yhteensä" value={good + bad + neutral} />
+            <Statistic text="keskiarvo" value={(good + bad * (-1) + neutral * 0) / (good + bad + neutral)} />
+            <Statistic text="positiivisia" value={good / (good + bad + neutral) * 100} pre='%'/>
         </div>
     ) : (
         <p>Ei yhtään palautetta annettu</p>
@@ -20,6 +28,14 @@ const Statistics = ({good, bad, neutral}) => {
             <h1>Statistiikka</h1>
             {statistics}
         </div>
+    )
+}
+
+const Button = ({text, set}) => {
+    return(
+        <span>
+            <button onClick={set}>{text}</button>
+        </span>
     )
 }
 
@@ -46,10 +62,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>Anna palautetta</h1>
-            <button onClick={() => addGood()}>hyvä</button>
-            <button onClick={() => addNeutral()}>neutraali</button>
-            <button onClick={() => addBad()}>huono</button>
+            <Button text="hyvä" set={() => addGood()}/>
+            <Button text="neutraali" set={() => addNeutral()}/>
+            <Button text="huono" set={() => addBad()}/>
             <Statistics good={good} bad={bad} neutral={neutral}/>
         </div>
     )
